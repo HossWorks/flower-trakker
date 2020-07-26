26fd8d220cd08db33314aec425318495cd05b8c7
Carl <carl.hostrander00@gmail.com>
2020-07-25 21:20:39 -0500

    initial app

---
+++ src/main/java/org/hoss/flowertrakker/FlowerTrakkerApplication.java
@@ -0,0 +1,13 @@
+package org.hoss.flowertrakker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerTrakkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerTrakkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hoss/flowertrakker/FlowerTrakkerApplicationTest.java
@@ -0,0 +1,13 @@
+package org.hoss.flowertrakker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerTrakkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
